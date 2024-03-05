4747a5e1d45b60d98e0f47813a54166ed5d7f16e
Kym Palacol <kympalacol@yahoo.com.ph>
2024-03-05 08:56:59 +0800

    startup-files

---
+++ my-portfolio/src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+
+  return (
+    <div>
+      <h1 className="text-4xl font-bold font-signature text-red-700">K<span className="text-black">y</span>m</h1>
+    </div>
+  )
+}
+
+export default App
